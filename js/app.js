8d1215d34217ef195e76d1848d3f5c3350b14f1f
Wojtek Pilich <chilip@tlen.pl>
2018-07-13 22:39:22 +0200

    trying different way to fetch and use data

--- js/app.js
+++ js/app.js
@@ -8,42 +8,102 @@ $(document).ready(function() {
     const arrow = $('#arrow');
 
     //fetching data from NBP api
-    function countResult (cur) {
-        $.ajax({
-            url: 'https://api.nbp.pl/api/exchangerates/rates/a/' + cur + '?format=json'
-        }).done(function(response) {
-            let currency = response['rates'][0].mid;
-            let amountVal = amount.val();
+    // function countResult (cur) {
+    //     $.ajax({
+    //         url: 'https://api.nbp.pl/api/exchangerates/rates/a/' + cur + '?format=json'
+    //     }).done(function(response) {
+    //         let rate = response['rates'][0].mid;
+    //         let amountVal = amount.val();
+    //
+    //         let result = amountVal + firstSelect.val() + ' = ' + (amountVal / rate).toFixed(2) + cur;
+    //
+    //         let view = $('<span class="output">');
+    //         view.text(result);
+    //
+    //         output.empty();
+    //         output.append(view);
+    //     });
+    // }
 
-            let result = amountVal + firstSelect.val() + ' = ' + (amountVal / currency).toFixed(2) + cur;
+    const rates = [];
 
-            let view = $('<span class="output">');
-            view.text(result);
-
-            output.empty();
-            output.append(view);
-        });
-    }
-//===============================================================================
-    count.on('click', function(e) {
-        e.preventDefault();
-
-        if(firstSelect.val() === 'pln') {
-            switch (secondSelect.val()) {
-                case 'eur':
-                    countResult('eur');
-                    break;
-                case 'usd':
-                    countResult('usd');
-                    break;
-                case 'chf':
-                    countResult('chf');
-                    break;
-                case 'gbp':
-                    countResult('gbp');
-                    break;
+    $.ajax({
+        url: 'https://api.nbp.pl/api/exchangerates/tables/A/?format=json'
+    }).done(function(data) {
+        for(value of data[0]['rates']) {
+            if(value.code.includes('GBP') || value.code.includes('EUR') ||
+                value.code.includes('CHF') || value.code.includes('USD') ) {
+                rates.push({
+                    code:value.code, rate:value.mid
+                });
             }
         }
+    });
+    console.log(rates);
+
+    function countResult(cur1) {
+        for(out of rates) {
+            // if (rar.code === cur1) {
+            //     // let result = amount.val() + firstSelect.val() +
+            //     //     ' = ' + (amount.val() / rar.rate).toFixed(2) + rar.code;
+            //     //
+            //     // let view = $('<span class="output">');
+            //     // view.text(result);
+            //     //
+            //     // output.empty();
+            //     // output.append(view);
+            // }
+            console.log(out);
+
+        }
+    }
+
+
+    count.on('click', function(e) {
+       e.preventDefault();
+
+       if(firstSelect.val() === 'pln') {
+           switch (secondSelect.val()) {
+               case 'eur':
+                   // let result = amount.val() + firstSelect.val() +
+                   //     ' = ' + (amount.val() / rates[1].rate).toFixed(2) + rates[1].code;
+                   //
+                   // let view = $('<span class="output">');
+                   //     view.text(result);
+                   //
+                   //     output.empty();
+                   //     output.append(view);
+                   countResult('eur')
+           }
+       }
+
+    });
+
+
+
+
+    // let result = countResult('eur');
+    // console.log(result);
+//===============================================================================
+//     count.on('click', function(e) {
+//         e.preventDefault();
+//
+//         if(firstSelect.val() === 'pln') {
+//             switch (secondSelect.val()) {
+//                 case 'eur':
+//                     countResult('eur');
+//                     break;
+//                 case 'usd':
+//                     countResult('usd');
+//                     break;
+//                 case 'chf':
+//                     countResult('chf');
+//                     break;
+//                 case 'gbp':
+//                     countResult('gbp');
+//                     break;
+//             }
+//         }
 
         // if(firstSelect.val() === 'eur') {
         //     switch (secondSelect.val()) {
@@ -55,7 +115,7 @@ $(document).ready(function() {
         // }
 
 
-    });
+    // });
 
 
 
